d3598a97eb760514993c624ffc3ec9ac83466958
stas_malikov <stas.malikov75@gmail.com>
2020-01-20 22:13:52 +0300

    корявые функции

--- code_generator.py
+++ code_generator.py
@@ -9,85 +9,113 @@ class CodeGenerator:
     def set_instructions(self):
         for node in self.tree.nodes:
             if node.type == "block_dot":
-                self.parce_dot(node.body)
+                self.parce_dot(node.body, 0)
 
-            # if node.type == "func_proc_block":
-            #     self.parce_func_proc(node.body)
+            if node.type == "func_proc_block":
+                self.parce_func_proc(node.body)
+
             
     def parce_func_proc(self, nodes):
         for node in nodes:
             if node.classtype == "function":
-                self.parce_func(node)
+                self.parce_func_proc_body(node, True)
             
             if node.classtype == "procedure":
-                self.parce_proc(node)
+                self.parce_func_proc_body(node, False)
 
+    def parce_func_proc_body(self, node, func):
+        instructoin = "def "+ node.name +"("
+        lengh = len(node.param_list_index)
+        for i in range(lengh):
+            if node.param_list_index[i] != "None":
+                tmp = self.tree.expr_list[node.param_list_index[i]]
+                for j in range(len(tmp.values)):
+                    if tmp.values[j] != "None":
+                        if (i == 0 ) and (j == 0):
+                            instructoin += tmp.values[j] + ","
 
-    def parce_dot(self, nodes):
+                        elif (i == lengh - 1) and (j == len(tmp.values) -1):
+                            instructoin += tmp.values[j]
+
+                        else:
+                            instructoin += " " + tmp.values[j] + ","
+        
+        instructoin += "):\n"
+        self.instructions.append(instructoin)
+        arr = []
+        for node_index in node.body:
+            if node_index != "None":
+                arr.append(self.tree.expr_list[node_index]) 
+        
+        self.parce_dot(arr, 1)
+        if func:
+            self.instructions.append(self.tab + "return " + node.name)
+
+    def parce_dot(self, nodes, multiplier):
         for node in nodes:
             if node.classtype == "ident":
                 continue
 
             if node.classtype == "assign":
-                self.parce_assign(node)
+                self.instructions.append(self.parce_assign(node, multiplier))
 
             if node.classtype == "if_block":
-                self.parce_if(node)
+                self.parce_if(node, multiplier)
 
             if node.classtype == "procedure_call":
-                self.parce_proc_call(node)
+                self.parce_proc_call(node, multiplier)
 
             if node.classtype == "for_block":
-                self.parce_for(node)
+                self.parce_for(node, multiplier)
                 
             if node.classtype == "while_block":
-                self.parce_while(node)
+                self.parce_while(node, multiplier)
 
             if node.classtype == "do_while_block":
-                self.parce_do_while(node)
+                self.parce_do_while(node, multiplier)
 
 
-    def parce_do_while(self, node):
+    def parce_do_while(self, node, multiplier):
         condition = self.tree.expr_list[node.condition_index]
-        instructoin = "while True:\n"
+        instructoin = multiplier*self.tab + "while True:\n"
         for index in node.body:
             if index != "None":
                 tmp_node = self.tree.expr_list[index]
-                instructoin +=  self.tab + self.choose_def(tmp_node) + "\n"
+                instructoin +=  self.tab*(multiplier + 1)  + self.choose_def(tmp_node) + "\n"
 
-        instructoin += self.tab + "if not " + self.choose_def(condition) + ":\n" + self.tab*2 + "break"
+        instructoin += self.tab*(multiplier + 1) + "if not " + self.choose_def(condition) + ":\n" + self.tab*(multiplier + 2) + "break"
         
         self.instructions.append(instructoin)
         return instructoin
 
 
-    def parce_while(self, node):
+    def parce_while(self, node, multiplier):
         condition = self.tree.expr_list[node.condition_index]
-        instructoin = "while " + self.choose_def(condition) + ":\n"
+        instructoin = multiplier*self.tab + "while " + self.choose_def(condition) + ":\n"
         for index in node.body:
             if index != "None":
                 tmp_node = self.tree.expr_list[index]
-                instructoin +=  self.tab + self.choose_def(tmp_node) + "\n"
+                instructoin +=  (multiplier + 1)*self.tab + self.choose_def(tmp_node) + "\n"
         
         self.instructions.append(instructoin)
         return instructoin
 
 
-    def parce_for(self, node):
+    def parce_for(self, node, multiplier):
         condition = self.tree.expr_list[node.start_condition_index]
         tmp_cond = self.choose_def(condition)
-        instructoin = tmp_cond + "\n" + "while " + tmp_cond.split()[0] + " <= " + str(node.end_number) + ":\n"
+        instructoin = multiplier*self.tab + tmp_cond + "\n" + multiplier*self.tab + "while " + tmp_cond.split()[0] + " <= " + str(node.end_number) + ":\n"
         for index in node.body:
             if index != "None":
                 tmp_node = self.tree.expr_list[index]
-                instructoin +=  self.tab + self.choose_def(tmp_node) + "\n"
+                instructoin +=  (multiplier + 1)*self.tab + self.choose_def(tmp_node) + "\n"
         
         self.instructions.append(instructoin)
         return instructoin
 
 
-    def parce_func_call(self, node):
-        instructoin = node.name + "("
+    def parce_func_call(self, node, multiplier):
+        instructoin = multiplier*self.tab + node.name + "("
         lengh = len(node.params)
         for i in range(lengh):
             if i == lengh - 1:
@@ -101,8 +129,8 @@ class CodeGenerator:
         return instructoin
 
 
-    def parce_proc_call(self, node):
-        instructoin = node.name + "("
+    def parce_proc_call(self, node, multiplier):
+        instructoin = multiplier*self.tab + node.name + "("
         lengh = len(node.params)
         for i in range(lengh):
             if i == lengh - 1:
@@ -118,8 +146,8 @@ class CodeGenerator:
 
             
 
-    def parce_if(self, node):
-        instructoin = "if "
+    def parce_if(self, node, multiplier):
+        instructoin = multiplier*self.tab + "if "
         condition = self.tree.expr_list[node.condition]
         instructoin += self.choose_def(condition) + ":" + "\n"
         if node.then_block is not None:
@@ -133,18 +161,17 @@ class CodeGenerator:
             for index in node.else_block:
                 if index != "None":
                     tmp_node = self.tree.expr_list[index]
-                    instructoin +=  self.tab + self.choose_def(tmp_node)
+                    instructoin +=  (multiplier + 1)*self.tab + self.choose_def(tmp_node)
 
         self.instructions.append(instructoin)
         return instructoin
 
 
 
-    def parce_assign(self, node):
-        instructoin = node.ident + " = "
+    def parce_assign(self, node, multiplier):
+        instructoin = multiplier*self.tab + node.ident + " = "
         body = self.tree.expr_list[node.body_index]
         instructoin += self.choose_def(body)
-        self.instructions.append(instructoin)
         return instructoin
         
 
@@ -183,7 +210,7 @@ class CodeGenerator:
         return instructon
             
 
-    def choose_def(self, node):
+    def choose_def(self, node, multiplier = 0):
         if node.classtype == "additive":
             return self.parce_additive(node)
 
@@ -191,17 +218,11 @@ class CodeGenerator:
             return self.parce_expr(node)
 
         if node.classtype == "assign":
-            return self.parce_assign(node)
+            return self.parce_assign(node, multiplier)
 
         if node.classtype == "function_call":
-            return self.parce_func_call(node)
+            return self.parce_func_call(node, multiplier)
 
-
-    def parce_func(self, node):
-        pass
-
-    def parce_proc(self, node):
-        pass
 
     def write(self):
         f = open('output.py', 'w')
--- output.py
+++ output.py
@@ -1,6 +1,17 @@
+def MaxNumber( a,b):
+
+    if a >= b:
+    max = -a
+
+def MinNumber( a,b):
+
+    if a <= b:
+    max = not a
+else:
+        max = b
+    MinNumber = 100
+    return MinNumber
 c = f - 40
-a = 20 + 30
-c = rr / 34
 if True | False:
     a = 20 + 30
 else:
@@ -9,16 +20,12 @@ MaxNumber(rr, tt)
 MinNumber()
 tt = MaxNumber(rr, tt)
 i = 0
-ttt = 55 * 100
-i = 0
 while i <= 10:
     ttt = 55 * 100
 
-i = 60 / 6
 while r > u:
     i = 60 / 6
 
-t = 50
 while True:
     t = 50
     if not i < 10:
